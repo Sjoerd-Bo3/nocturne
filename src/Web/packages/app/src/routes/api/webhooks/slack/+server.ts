defbbf1ec253166bec683127b8e603938547ea59
Rhys <36913739+ryceg@users.noreply.github.com>
2026-03-22 19:00:59 +1100

    feat(bot): add webhook proxy routes for chat platforms

---
+++ src/Web/packages/app/src/routes/api/webhooks/slack/+server.ts
@@ -0,0 +1,16 @@
+import type { RequestHandler } from "./$types";
+import { env } from "$env/dynamic/private";
+
+const BOT_URL = env.BOT_WEBHOOK_URL ?? "http://localhost:3001";
+
+export const POST: RequestHandler = async ({ request }) => {
+	const res = await fetch(`${BOT_URL}/webhooks/slack`, {
+		method: "POST",
+		headers: request.headers,
+		body: await request.arrayBuffer(),
+	});
+	return new Response(res.body, {
+		status: res.status,
+		headers: res.headers,
+	});
+};
